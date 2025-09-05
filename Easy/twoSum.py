98908d1dfe8b02f7f97fdf27889ce69319a6f732
Rahul Tevatia <rtevatia2@gmail.com>
2025-09-05 14:46:44 -0400

    two sum problem - level: easy

--- Easy/twoSum.py
+++ Easy/twoSum.py
@@ -30,3 +30,26 @@ Constraints:
 Only one valid answer exists.
 
 """
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    return [i, j]
+        return []
+    
+def main():
+    solution = Solution()
+    print(solution.twoSum([2,7,11,15], 9))
+    print(solution.twoSum([3,2,4], 6))
+    print(solution.twoSum([3,3], 6))
+    print(solution.twoSum([1,2,3,4], 100))
+
+if __name__ == "__main__":
+    main()
